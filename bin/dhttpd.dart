055cdfd0fb945ce765299e082598a7d79f66922f
Juan Galindo <33429312+JuanGalindoUniters@users.noreply.github.com>
2023-10-05 13:03:10 -0400

    Add nullsafety

--- bin/dhttpd.dart
+++ bin/dhttpd.dart
@@ -20,15 +20,9 @@ Future<void> main(List<String> args) async {
   }
   
   
-  final herokuPort = TryParseNullable(Platform.environment['PORT']);
+  final herokuPort = int.tryParse(Platform.environment['PORT']!);
   
   await Dhttpd.start(path: options.path, port: herokuPort ?? 8080, address: options.host);
 
   print('Server started on port ${herokuPort ?? 8080}');
-}
-
-public int? TryParseNullable(string val)
-{
-  int outValue;
-  return int.TryParse(val, out outValue) ? (int?)outValue : null;
 }
